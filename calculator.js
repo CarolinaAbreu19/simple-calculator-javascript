2149ed5cad67890a4d02cbe6e9b870f3664544a7
Carolina Abreu <anacarolinaks19@gmail.com>
2022-01-17 20:29:08 -0300

    feat: value of display

--- calculator.js
+++ calculator.js
@@ -1,28 +1,3 @@
-var values, answer;
-var checkAnswer = false;
+const display = document.querySelector(".calculator__display");
 
-function insertNumber(num) {
-    if (checkAnswer == true) {
-        clearDisplay();
-        checkAnswer = false;
-    }
-
-    values = document.calculator.screen.value += num;
-}
-
-function insertOperator(num) {
-    if (checkAnswer == true) {
-        checkAnswer = false;
-    }
-    values = document.calculator.screen.value += num;
-}
-
-function performOperation() {
-    answer = eval(values);
-    document.calculator.screen.value = answer;
-    checkAnswer = true;
-}
-
-function clearDisplay() {
-    document.calculator.screen.value = "";
-}
+display.textContent = "0";
