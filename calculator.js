82391363889d968a70aab33a5386b43f71a7ee78
Carolina Abreu <anacarolinaks19@gmail.com>
2022-01-18 11:02:36 -0300

    feat: clear button

--- calculator.js
+++ calculator.js
@@ -4,3 +4,23 @@ const numButtons = document.querySelectorAll(".--number");
 const operatorButtons = document.querySelectorAll(".--operator");
 const answerButton = document.querySelector(".--answer");
 const clearButton = document.querySelector(".--clear");
+
+const entry = {
+    firstNum: 0,
+    secondNum: 0,
+    operation: null,
+    result: 0
+}
+
+let firstNumChoose = false;
+let secondNumChoose = false;
+
+clearButton.addEventListener("click", () => {
+    display.value = 0;
+    entry.firstNum = 0;
+    entry.secondNum = 0;
+    entry.operation = null;
+    entry.result = 0;
+    firstNumChoose = false;
+    secondNumChoose = false;
+});
